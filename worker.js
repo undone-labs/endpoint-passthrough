1c18483247634de22ba441f2433bbf317dedc427
Orun Bhuiyan <orun@agencyundone.com>
2023-07-01 20:51:19 -0400

    feat: add better error handling to url behavior

--- worker.js
+++ worker.js
@@ -6,34 +6,40 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request, allowedHostnames) {
-  const url = new URL(request.url)
-  let apiURL = url.searchParams.get('api')
+  try {
+    const url = new URL(request.url);
+    let apiURL = url.searchParams.get('api');
 
-  if (!apiURL || !endpoints.includes(apiURL)) {
-    return new Response('Invalid API URL', {status: 403})
-  }
-
-  // Handle hostname checking
-  const hostnameCheckResponse = handleHostname(request, allowedHostnames);
-  if (hostnameCheckResponse) {
-    return hostnameCheckResponse;
-  }
-
-  const cache = caches.default
-  let response = await cache.match(request)
-
-  if (!response) {
-    let req = new Request(apiURL, request)
-    req.headers.set('Authorization', `Bearer ${process.env[apiURL]}`)
-
-    response = await fetch(req)
-
-    if (response.ok) {
-      let responseClone = response.clone()
-      responseClone.headers.append('Cache-Control', `public, max-age=${cacheDurationSeconds}`)
-      event.waitUntil(cache.put(request, responseClone))
+    // Handle hostname checking
+    const hostnameCheckResponse = handleHostname(request, allowedHostnames);
+    if (hostnameCheckResponse) {
+      return hostnameCheckResponse;
     }
-  }
 
-  return response
+    if (!apiURL || !endpoints.includes(apiURL)) {
+      throw new Error('Invalid API URL');
+    }
+
+    const cache = caches.default;
+    let response = await cache.match(request);
+
+    if (!response) {
+      let req = new Request(apiURL, request);
+      req.headers.set('Authorization', `Bearer ${process.env[apiURL]}`);
+
+      response = await fetch(req);
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch from API: ${response.statusText}`);
+      }
+
+      let responseClone = response.clone();
+      responseClone.headers.append('Cache-Control', `public, max-age=${cacheDurationSeconds}`);
+      event.waitUntil(cache.put(request, responseClone));
+    }
+
+    return response;
+  } catch (err) {
+    return new Response(err.message, { status: 500 });
+  }
 }
